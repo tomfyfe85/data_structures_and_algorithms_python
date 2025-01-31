2dd6f4611eb524a3d3cb8b890b1c2f86287d7486
tomfyfe <tomfyfe85@gmail.com>
2025-01-31 21:06:42 +0000

    added big_0.txt

--- part_1/lists.py
+++ part_1/lists.py
@@ -2,15 +2,27 @@
 
 
 
-def find_min_index(nums):
-	smallest = nums[0]
-	index_of_smallest = 0
+# def find_min_index(nums):
+# 	smallest = nums[0]
+# 	index_of_smallest = 0
+#
+# 	for k, v in enumerate(nums[1:]):
+# 		if v < smallest:
+# 			smallest = v
+# 			index_of_smallest = k
+#
+# 	return index_of_smallest
+#
+# print(find_min_index([10, 20, 5, 3, 8]))
 
-	for k, v in enumerate(nums[1:]):
-		if v < smallest:
-			smallest = v
-			index_of_smallest = k
 
-	return index_of_smallest
 
-print(find_min_index([10, 20, 5, 3, 8]))
+def remove_duplicates(nums):
+	no_dups = []
+	for num in nums:
+	 	if num not in no_dups:
+			no_dups.append(num)
+	return no_dups
+
+
+print(remove_duplicates([1, 2, 2, 3, 4, 4, 5]))
